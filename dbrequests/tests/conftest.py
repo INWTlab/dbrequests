e5652b7098250f745ce8ef9df8cdba55b1ad4299
phainom <mdeutsch@outlook.com>
2020-03-04 22:28:53 +0100

    11 - default values to test table

--- dbrequests/tests/conftest.py
+++ dbrequests/tests/conftest.py
@@ -44,8 +44,8 @@ def set_up_table(db):
         CREATE TABLE cats
             (
               id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
-              name            VARCHAR(150) NOT NULL,                # Name of the cat
-              owner           VARCHAR(150) NOT NULL,                # Owner of the cat
+              name            VARCHAR(150) NOT NULL DEFAULT '',                # Name of the cat
+              owner           VARCHAR(150) NOT NULL DEFAULT '',                # Owner of the cat
               birth           DATE NOT NULL,                        # Birthday of the cat
               PRIMARY KEY     (id)                                  # Make the id the primary key
             );
