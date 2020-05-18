0acd6a900817865ce15d3b7e5fd1dc7e6e463a55
Sebastian Warnholz <wahani@gmail.com>
2020-05-18 12:45:34 +0200

    added test cases for multiple columns in_set + in_join

--- dbrequests/mysql/tests/test_send_delete.py
+++ dbrequests/mysql/tests/test_send_delete.py
@@ -6,7 +6,9 @@ from dbrequests.mysql.tests.conftest import (
     set_up_diffs as reset_diffs)
 
 
-@pytest.mark.usefixtures('db')
+pytestmark = pytest.mark.usefixtures('db')
+
+
 class TestSendDeleteInSet:
     """Tests for mode=in_set."""
 
@@ -30,8 +32,30 @@ class TestSendDeleteInSet:
         assert res.shape == (1, 3)
         assert res[0, 'id'] == 3
 
+    def test_delete_with_multiple_cols(self, db):
+        """Delete entries from a table identified by multiple columns."""
+        reset_diffs(db)
+        df = dt.Frame({
+            'id': [1, 2, 3, 4],
+            'value': ['a', 'b', 'b', 'c']
+        })
 
-@pytest.mark.usefixtures('db')
+        df_delete = dt.Frame({
+            # ids we want to delete:
+            'id': [2, 3],
+            # in set means in 'id' and in 'value', so we expect that both rows
+            # will be deleted, because we have a value in the space of allowed
+            # values. This is the difference compared to in_join!
+            'value': ['b', 'c']
+        })
+
+        db.send_data(df, 'diffs')
+        nrows = db.send_delete(df_delete, 'diffs', 'in_set')
+        res = db.send_query('select * from diffs', to_pandas=False)
+        assert nrows == 2
+        assert res.shape == (2, 3)
+
+
 class TestSendDeleteNotInSet:
     """Tests for mode=not_in_set."""
 
@@ -57,7 +81,6 @@ class TestSendDeleteNotInSet:
         assert res[1, 'id'] == 2
 
 
-@pytest.mark.usefixtures('db')
 class TestSendDeleteInJoin:
     """Tests for mode=in_join."""
 
@@ -81,8 +104,30 @@ class TestSendDeleteInJoin:
         assert res.shape == (1, 3)
         assert res[0, 'id'] == 3
 
+    def test_delete_with_multiple_cols(self, db):
+        """Delete entries from a table identified by multiple columns."""
+        reset_diffs(db)
+        df = dt.Frame({
+            'id': [1, 2, 3, 4],
+            'value': ['a', 'b', 'b', 'c']
+        })
 
-@pytest.mark.usefixtures('db')
+        df_delete = dt.Frame({
+            # ids we want to delete:
+            'id': [2, 3],
+            # in_join means rows are identified as in the df we submit. So we
+            # expect that only one row will be deleted. This is different from
+            # in_set!
+            'value': ['b', 'c']
+        })
+
+        db.send_data(df, 'diffs')
+        nrows = db.send_delete(df_delete, 'diffs', 'in_join')
+        res = db.send_query('select * from diffs', to_pandas=False)
+        assert nrows == 1
+        assert res.shape == (3, 3)
+
+
 class TestSendDeleteNotInJoin:
     """Tests for mode=not_in_join."""
 
